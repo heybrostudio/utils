52b9ea39d09371c490806209e592f4bd10799c9e
Caven <cavenasdev@gmail.com>
2024-04-05 09:02:30 +0800

    feat: add gray for console

--- src/console.ts
+++ src/console.ts
@@ -26,3 +26,12 @@ export function cyan(text: string): string {
 export function red(text: string): string {
 	return `\x1b[31m${text}\x1b[0m`
 }
+
+/**
+ * Print gray text
+ * @param text Text you want to print
+ * @returns Output gray text
+ */
+export function gray(text: string): string {
+	return `\x1b[90m${text}\x1b[0m`
+}
