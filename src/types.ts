b5b66179bb9c68f658e43bc3c74b422cc489f9be
Caven <cavenasdev@gmail.com>
2024-04-12 20:11:47 +0800

    feat: add IsAny and Callback types for types

--- src/types.ts
+++ src/types.ts
@@ -9,3 +9,21 @@ export type Func<T = void> = () => T
 export type Flatten<T> = {
 	[K in keyof T]: T[K] extends object ? Flatten<T[K]> : T[K]
 } & {}
+
+/**
+ * IsAny
+ *
+ * @see
+ * The source code for this type was inspired by vueuse's `IsAny` type util
+ * https://github.com/vueuse/vueuse/blob/1558cd2b5b019abc1feda6d702caa1053a182903/packages/shared/utils/types.ts#L155C4-L155C60
+ */
+export type IsAny<T> = 0 extends 1 & T ? true : false
+
+/**
+ * Callback function type
+ */
+export type Callback<T, R = void> = IsAny<T> extends true
+	? (param: any) => R
+	: [T] extends [void]
+		? () => R
+		: (param: T) => R
